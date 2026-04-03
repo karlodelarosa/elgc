364d318d7815dfcbca78ed5510fd7f75f52cf086
Karlo Dela Rosa <jrosa@salespond.com>
2026-04-03 18:04:29 +0800

    feat: added meta date

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -5,6 +5,37 @@ import Image from 'next/image';
 import { Calendar, MapPin } from 'lucide-react';
 import { Blobs } from '@/app/components/ui/blobs/blobs.component';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'ALAB: Illuminate | ELGC Church',
+  description:
+    "ALAB: Illuminate is a fundraising event supporting Family Day participants and church renovation at Emmanuel's Living Gospel Church.",
+
+  openGraph: {
+    title: 'ALAB: Illuminate',
+    description: 'Fundraising event supporting Family Day participants and church renovation.',
+    url: 'https://elgchurch.com/events/alab-illuminate',
+    siteName: "Emmanuel's Living Gospel Church",
+    images: [
+      {
+        url: 'https://elgchurch.com/img/alab-tix.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'ALAB: Illuminate Event',
+      },
+    ],
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ALAB: Illuminate',
+    description: 'Fundraising event supporting Family Day participants and church renovation.',
+    images: ['https://elgchurch.com/img/alab-tix.jpeg'],
+  },
+};
+
 export default function AlabEventPage() {
   return (
     <div className="relative px-6 md:px-12 py-14 max-w-6xl mx-auto space-y-12">
